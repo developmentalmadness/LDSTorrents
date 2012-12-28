523e2b3a8f44b715bb00fe3eaa1a34f483ce310b
Mark J. Miller <developmentalmadness@gmail.com>
2012-12-28 05:06:44 -0700

    Genesis

---
+++ scripts/CREATE DATABASE.sql
@@ -0,0 +1,79 @@
+IF OBJECT_ID(N'Torrents') IS NOT NULL
+	DROP TABLE Torrents
+GO
+
+CREATE TABLE Torrents (
+	TorrentID INT IDENTITY(1,1) PRIMARY KEY,
+	ChannelID INT NOT NULL,
+	Title VARCHAR(256) NOT NULL,
+	ResourceUri VARCHAR(1024) NOT NULL,
+	TorrentUri VARCHAR(1024) NOT NULL,
+	LocalPath VARCHAR(1024) NOT NULL
+)
+
+IF OBJECT_ID(N'Channels') IS NOT NULL
+	DROP TABLE Channels
+GO
+
+CREATE TABLE Channels (
+	ChannelID INT IDENTITY(1,1) PRIMARY KEY,
+	Title VARCHAR(256) NOT NULL,
+	Host VARCHAR(256) NOT NULL,
+	Url VARCHAR(1024) NOT NULL,
+	Category VARCHAR(256) NOT NULL
+)
+
+INSERT INTO Channels (Title, Host, Url, Category) VALUES
+('Mormon Messages','mormonchannel.org','http://www.mormonchannel.org/mormonmessages','Series'),
+('Bible Videos','mormonchannel.org','http://www.mormonchannel.org/biblevideos','Series'),
+('Conversations','mormonchannel.org','http://www.mormonchannel.org/conversations','Series'),
+('Enduring It Well','mormonchannel.org','http://www.mormonchannel.org/enduring-it-well','Series'),
+('Gospel Solutions for Families','mormonchannel.org','http://www.mormonchannel.org/gospel-solutions-for-families','Series'),
+('Mormon Channel Daily','mormonchannel.org','http://www.mormonchannel.org/mormon-channel-daily','Series'),
+('Relief Society','mormonchannel.org','http://www.mormonchannel.org/relief-society','Series'),
+('Stories from General Conference','mormonchannel.org','http://www.mormonchannel.org/stories-from-general-conference','Series'),
+('Tech Savvy','mormonchannel.org','http://www.mormonchannel.org/tech-savvy','Series'),
+('Teaching, No Greater Call','mormonchannel.org','http://www.mormonchannel.org/teaching-no-greater-call','Series'),
+('We Will Stand','mormonchannel.org','http://www.mormonchannel.org/we-will-stand','Series'),
+('Weekly Edition','mormonchannel.org','http://www.mormonchannel.org/weekly-edition','Series'),
+('Mormon Messages For Youth','mormonchannel.org','http://www.mormonchannel.org/mormon-messages-for-youth','Series'),
+('I''m a Mormon ','mormonchannel.org','http://www.mormonchannel.org/our-people','Series'),
+('Archives ','mormonchannel.org','http://www.mormonchannel.org/archives','Series'),
+('O Come, Emmanuel - Christmas Version - ThePianoGuys','mormonchannel.org','http://www.mormonchannel.org/more-videos?v=2008190983001','Collections'),
+('First Presidency Christmas Devotional ','mormonchannel.org','http://www.mormonchannel.org/christmas-devotional-2012','Collections'),
+('A Christmas Carol','mormonchannel.org','http://www.mormonchannel.org/a-christmas-carol','Collections'),
+('The Coat','mormonchannel.org','http://www.mormonchannel.org/the-coat?v=1318641007001','Collections'),
+('The Holy Spirit ','mormonchannel.org','http://www.mormonchannel.org/power-of-the-holy-spirit','Collections'),
+('CES Devotionals ','mormonchannel.org','http://www.mormonchannel.org/ces-devotionals','Collections'),
+('Addiction Recovery Program','mormonchannel.org','http://www.mormonchannel.org/addiction-recovery-program','Collections'),
+('Provident Living','mormonchannel.org','http://www.mormonchannel.org/provident-living','Collections'),
+('Mormon Channel Videos','mormonchannel.org','http://www.mormonchannel.org/more-videos','Collections'),
+('Mormon Messages For Youth','mormonchannel.org','http://www.mormonchannel.org/mormon-messages-for-youth','Youth'),
+('For the Youth','mormonchannel.org','http://www.mormonchannel.org/for-the-youth','Youth'),
+('Youth Voices','mormonchannel.org','http://www.mormonchannel.org/youth-voices','Youth'),
+('Youth Theme 2012: Arise and Shine Forth ','mormonchannel.org','http://www.mormonchannel.org/youth-theme-2012','Youth'),
+('The Coat','mormonchannel.org','http://www.mormonchannel.org/more-videos?v=1318641007001','Children'),
+('The Story of Jesus'' Birth','mormonchannel.org','http://www.mormonchannel.org/weekly-edition/14?v=2050186924001','Children'),
+('Scripture Stories','mormonchannel.org','http://www.mormonchannel.org/scripture-stories','Children'),
+('Book of Mormon Stories','mormonchannel.org','http://www.mormonchannel.org/book-of-mormon-stories','Children'),
+('Old Testament Stories ','mormonchannel.org','http://www.mormonchannel.org/old-testament-stories','Children'),
+('New Testament Stories ','mormonchannel.org','http://www.mormonchannel.org/new-testament-stories','Children'),
+('Doctrine and Covenants Stories','mormonchannel.org','http://www.mormonchannel.org/doctrine-and-covenants-stories','Children'),
+('Old Testament','mormonchannel.org','http://www.mormonchannel.org/scriptures/ot','Scriptures and Magazines'),
+('New Testament','mormonchannel.org','http://www.mormonchannel.org/scriptures/nt','Scriptures and Magazines'),
+('Book of Mormon','mormonchannel.org','http://www.mormonchannel.org/scriptures/bofm','Scriptures and Magazines'),
+('Doctrine and Covenants','mormonchannel.org','http://www.mormonchannel.org/scriptures/dc-testament','Scriptures and Magazines'),
+('Pearl of Great Price','mormonchannel.org','http://www.mormonchannel.org/scriptures/pgp','Scriptures and Magazines'),
+('Ensign','mormonchannel.org','http://www.mormonchannel.org/magazines/ensign','Scriptures and Magazines'),
+('New Era','mormonchannel.org','http://www.mormonchannel.org/magazines/new-era','Scriptures and Magazines'),
+('Friend','mormonchannel.org','http://www.mormonchannel.org/magazines/friend','Scriptures and Magazines')
+
+
+
+
+
+
+
+
+
+
---
+++ src/LDSTorrents/Program.cs
@@ -0,0 +1,199 @@
+﻿using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+using MonoTorrent.Common;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Web;
+using Massive;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO.Compression;
+
+namespace LDSTorrents
+{
+    class Program
+    {
+        static dynamic Torrents = new DynamicModel("LDSTorrents", tableName: "Torrents", primaryKeyField: "TorrentID");
+
+        static void Main(string[] args)
+        {
+            //ScrapeChannel();
+            //ScrapeCategories();
+            ScrapeChannels();
+        }
+
+        private static void ScrapeChannels()
+        {
+            var table = new DynamicModel("LDSTorrents", tableName: "Channels", primaryKeyField: "ChannelID");
+
+            var channels = table.All();
+            foreach (var channel in channels)
+            {
+                Console.WriteLine("Scraping '{0}'...", channel.Title);
+                var videos = ScrapeChannel(channel);
+                Torrents.Save(videos.ToArray());
+                break;
+            }
+        }
+
+        private static void ScrapeCategories()
+        {
+            string url = "http://www.mormonchannel.org/";
+            var request = WebRequest.CreateHttp(url);
+
+            HtmlDocument html = new HtmlDocument();
+            using (var input = request.GetResponse())
+                html.Load(input.GetResponseStream(), true);
+
+            var doc = html.DocumentNode;
+
+            var categories = doc.QuerySelectorAll(".ribbon");
+            foreach (var item in categories)
+            {
+                var category = item.QuerySelector(".ribbon-title").InnerText;
+                category = category.Substring(0, category.IndexOf("&#160;")).TrimEnd();
+
+                var channels = item.QuerySelectorAll(".teaser_title");
+                foreach (var channel in channels)
+                {
+                    var name = channel.InnerText;
+                    var location = channel.GetAttributeValue("href", String.Empty);
+                    Console.WriteLine("('{0}','mormonchannel.org','{1}','{2}'),", name, location, category);
+                }
+            }
+        }
+
+        private static IEnumerable<dynamic> ScrapeChannel(dynamic channel)
+        {
+            var list = new List<dynamic>();
+
+            var request = WebRequest.CreateHttp(channel.Url);
+
+            HtmlDocument html = new HtmlDocument();
+            using (var input = request.GetResponse())
+                html.Load(input.GetResponseStream(), true);
+
+            var doc = html.DocumentNode;
+
+            var videos = doc.QuerySelectorAll(".video");
+            foreach (var video in videos)
+            {
+                var urls = video.QuerySelectorAll(".download_links  a");
+                foreach (var item in urls)
+                {
+                    if (String.Compare(item.InnerText, "mp4", true) == 0)
+                    {
+                        var title = HttpUtility.HtmlDecode(video.QuerySelector("a.teaser_title").InnerText);
+                        var resource = item.GetAttributeValue("href", String.Empty);
+                        var exists = Torrents.First(ResourceUri: resource);
+                        if (exists != null)
+                            continue;
+
+                        Console.WriteLine("{0} ==> {1}", title, resource);
+
+                        string result = String.Empty;
+                        string rh = String.Empty;
+                        string torrentUrl = String.Empty;
+
+                        while (true)
+                        {
+                            var torrentRequest = WebRequest.CreateHttp("http://burnbit.com/regfile");
+                            torrentRequest.Method = "POST";
+                            torrentRequest.ContentType = "application/x-www-form-urlencoded";
+                            torrentRequest.Accept = "application/json; charset=utf-8";
+
+                            switch (result)
+                            {
+                                case "":
+                                    using (StreamWriter writer = new StreamWriter(torrentRequest.GetRequestStream()))
+                                        writer.Write(String.Format("file={0}", HttpUtility.UrlEncode(resource)));
+                                    break;
+                                case "wait":
+                                    using (StreamWriter writer = new StreamWriter(torrentRequest.GetRequestStream()))
+                                        writer.Write(String.Format("rh={0}", rh));
+                                    break;
+                                default:
+                                    Console.WriteLine("Oops! What happened? '{0}'", result);
+                                    break;
+                            }
+
+                            string json = String.Empty;
+                            using (StreamReader reader = new StreamReader(torrentRequest.GetResponse().GetResponseStream()))
+                                json = reader.ReadToEnd();
+
+                            var response = JObject.Parse(json);
+                            result = response.GetValue("status").Value<String>();
+
+                            if ("|success|exists|".IndexOf(result) != -1)
+                            {
+                                torrentUrl = String.Format("http://burnbit.com{0}", response.GetValue("redirect").Value<String>().Replace("/torrent/", "/download/"));
+                                Console.WriteLine("Torrent: {0}", torrentUrl);
+                                break;
+                            }
+                            else if (result == "error")
+                            {
+                                //TODO: if error includes "not resumable" I'll need to create the torrent manually: Download the file, create the torrent, upload the torrent (via ftp) to a hosted url, then add it to the feed
+                                var error = response.GetValue("html").Value<String>();
+                                Console.WriteLine("Error: {0}", error);
+                                break;
+                            }
+
+                            rh = response.GetValue("rh").Value<String>();
+                            Console.WriteLine("Waiting (rh: {0})...", rh);
+                            Thread.Sleep(5000);
+                        }
+
+                        if (torrentUrl.Length == 0)
+                            continue;
+
+                        var fileName = String.Format("{0}.torrent", torrentUrl.Substring(torrentUrl.LastIndexOf("/") + 1));
+                        string localPath = String.Format("torrents{4}{0}{4}{1}{4}{2}{4}{3}", channel.Host, channel.Category, channel.Title, fileName, Path.DirectorySeparatorChar);
+                        localPath = localPath.Replace(' ', '_').Replace(",", String.Empty).Replace("'", String.Empty);
+                        string filePath = Path.Combine(Environment.CurrentDirectory, localPath);
+
+                        if (!Directory.Exists(filePath))
+                        {
+                            var directory = Path.GetDirectoryName(filePath);
+                            Directory.CreateDirectory(directory);
+                        }
+
+                        //download torrent
+                        Console.WriteLine("Downloading torrent file '{0}'", torrentUrl);
+                        var getTorrent = WebRequest.CreateHttp(torrentUrl);
+                        getTorrent.Method = "GET";
+                        getTorrent.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip");
+
+                        using (var downloadTorrent = new GZipStream(getTorrent.GetResponse().GetResponseStream(), CompressionMode.Decompress))
+                        {
+                            
+                            using (var torrentFile = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                            {
+                                byte[] torrentBuffer = new byte[4096];
+                                int bytesRead = -1;
+                                while (0 < (bytesRead = downloadTorrent.Read(torrentBuffer, 0, torrentBuffer.Length)))
+                                    torrentFile.Write(torrentBuffer, 0, bytesRead);
+                            }
+                        }
+
+                        list.Add(new { 
+                            ChannelID = channel.ChannelID,
+                            Title = title,
+                            TorrentUri = torrentUrl,
+                            ResourceUri = resource,
+                            LocalPath = String.Format("{0}{1}", Path.DirectorySeparatorChar, localPath)
+                        });
+                    }
+                }
+
+                if(list.Count != 0)
+                    break;
+            }
+
+            return list;
+        }
+    }
+}
