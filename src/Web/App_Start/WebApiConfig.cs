25cc404cc02bec296bf85479121b4d220e15fecb
Mark J. Miller <developmentalmadness@gmail.com>
2012-12-28 05:50:13 -0700

    Adding web application

---
+++ src/Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Web.Code;
+
+namespace Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            GlobalConfiguration.Configuration.Formatters.Add(new SyndicationFeedFormatter());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/Web/Models/Url.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class Url
+    {
+        public int UrlId { get; set; }
+        public string Address { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
